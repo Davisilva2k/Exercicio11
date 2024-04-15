82160adcbfa364f849630982daa5addbb84b49e8
Davisilva2k <davigraff05@gmail.com>
2024-04-14 22:42:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('Claro').addEventListener('click', function() {
+    document.documentElement.style.setProperty('--cor-primaria', 'green');
+    document.documentElement.style.setProperty('--cor-secundaria', 'gray');
+
+  });
+
+  document.getElementById('Escuro').addEventListener('click', function() {
+    document.documentElement.style.setProperty('--cor-primaria', '#282c34');
+    document.documentElement.style.setProperty('--cor-secundaria', '#c7d2fe');
+
+  });
+
+  
